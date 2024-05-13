0c91a1ef98331ad9d6d19b0566f0ebd2b43f696c
betselotT <betselottesfa9@gmail.com>
2024-05-13 17:10:34 -0300

    first

---
+++ src/Player.svelte
@@ -0,0 +1,25 @@
+<style>
+    .player {
+        flex-grow: 1;
+    }
+    .plus {
+        background-color: seagreen;
+    }
+    .minus {
+        background-color: brown;
+    }
+    button {
+        font-size: 20px;
+        border-radius: 3px;
+        width: 40px;
+        color: white;
+        font-family: monospace;
+        font-weight: bold;
+    }
+</style>
+
+<div class="player">
+    <h2>10</h2>
+    <button class="plus">+</button>
+    <button class="minus">-</button>
+</div>
